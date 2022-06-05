008cdf935c2c85c826dde2902812e54c145c5c04
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-05 00:00:30 -0300

    Recuperando dados de formulario e imprimendo em tabela

--- acoes.php
+++ acoes.php
@@ -8,6 +8,16 @@ function home(){
     include 'telas/home.php';
 }
 function cadastro(){
+    if($_POST){
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    $arquivo = fopen('exercicio-crudContatos/dados/contatos.csv','a+');
+    
+    fwrite($arquivo, "{$nome};{$email};{$telefone}".PHP_EOL);
+    
+    }
     include 'telas/cadastro.php';
 }
 function erro404(){
--- telas/cadastro.php
+++ telas/cadastro.php
@@ -4,9 +4,9 @@
             <h1>Cadastro</h1>
             <hr>
             <form action="" method="post" style="width:320px;" class="bg-secondary bg-gradient bg-opacity-25 rounded-2 p-4 border-0">
-                <input type="text" class="form-control" placeholder="Digite seu nome"><br><br>
-                <input type="text" class="form-control"  placeholder="Digite seu email"><br><br>
-                <input type="password" class="form-control" placeholder="Digite sua senha"><br><br>
+                <input type="text" name="nome" class="form-control" placeholder="Digite seu nome"><br><br>
+                <input type="text" name="email" class="form-control"  placeholder="Digite seu email"><br><br>
+                <input type="text" name="telefone" class="form-control" placeholder="Digite seu telefone"><br><br>
                 <button type="submit" class="btn btn-warning ">Enviar</button>
             </form>
         </main>
